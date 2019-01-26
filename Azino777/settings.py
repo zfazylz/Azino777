d666c66cbb6d1ea0bf33b7b0ab3631dfcbee3ab9
zfazylz <faza.zheten123@gmail.com>
2019-01-26 19:56:39 +0600

    no sign up fix

--- Azino777/settings.py
+++ Azino777/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '(t^atm^rr7o=3hy!^(wb^sahvxs@c95ohrdz=h$c$_cu!n%&_k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
